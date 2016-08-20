22c29efe4c56add6bd23a4f34e3e3b3094b8321f
Chris W Jones <chris@christopherjones.us>
2016-08-20 21:29:35 +0000

    Finished up project
    This was pretty easy since the `req` object from ExpressJS contained
    most of the data I needed it was simply slicing and dicing up strings.

--- app/controllers/headerParser.server.js
+++ app/controllers/headerParser.server.js
@@ -4,8 +4,22 @@
 function HeaderParser () {
 
 	this.whoami = function (req, res) {
+		var userInfo = {ipaddress:'',language:'',software:''};
 
-				res.json("hi");
+		var acceptLanguageString = req.get('accept-language');
+		var languageComma = acceptLanguageString.indexOf(',');
+		var userLanguage = acceptLanguageString.substring(0, languageComma);
+
+		var userAgentString = req.get('user-agent');
+		var openParens = userAgentString.indexOf('(') + 1; // Skip the parens
+		var closeParens = userAgentString.indexOf(')');
+		var operatingSystem = userAgentString.substring(openParens,closeParens);
+
+		userInfo.ipaddress = req.get('x-forwarded-for');
+		userInfo.language = userLanguage;
+		userInfo.software = operatingSystem;
+
+		res.json(userInfo);
 	};
 }
 
